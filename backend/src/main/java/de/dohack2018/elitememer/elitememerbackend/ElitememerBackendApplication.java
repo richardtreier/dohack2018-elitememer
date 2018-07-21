9e6bd33bd73b89fa043ac61cfdc8d9a212b8e8ed
Richard Treier <richard.treier@gmail.com>
2018-07-21 11:04:17 +0200

    Initial Backend Commit: Spring initializr

---
+++ backend/src/main/java/de/dohack2018/elitememer/elitememerbackend/ElitememerBackendApplication.java
@@ -0,0 +1,12 @@
+package de.dohack2018.elitememer.elitememerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElitememerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElitememerBackendApplication.class, args);
+	}
+}
