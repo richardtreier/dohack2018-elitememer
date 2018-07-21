8dac8b7280485a0c1ada95d3a4fd8e5329593c4b
Richard Treier <richard.treier@gmail.com>
2018-07-21 12:20:21 +0200

    Improved AbstractBaseRepository.

--- backend/src/main/java/de/dohack2018/elitememer/elitememerbackend/dao/repositories/AbstractBaseRepository.java
+++ backend/src/main/java/de/dohack2018/elitememer/elitememerbackend/dao/repositories/AbstractBaseRepository.java
@@ -2,10 +2,13 @@ package de.dohack2018.elitememer.elitememerbackend.dao.repositories;
 
 import de.dohack2018.elitememer.elitememerbackend.dao.entities.AbstractBaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.UUID;
 
 @NoRepositoryBean
-public interface AbstractBaseRepository<T extends AbstractBaseEntity> extends JpaRepository<T, UUID> {
+public interface AbstractBaseRepository<T extends AbstractBaseEntity> extends JpaRepository<T, UUID>,
+        CrudRepository<T, UUID>, JpaSpecificationExecutor<T> {
 }
